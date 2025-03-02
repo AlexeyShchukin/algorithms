41ef730baa8d6ec3f98a0a47ed9eab77224d0872
AlexeyShchukin <shchukin.developer@gmail.com>
2025-03-02 16:34:47 +0100

    two_sum3

--- two_sum_3.py
+++ two_sum_3.py
@@ -1,16 +1,17 @@
-"""Дан отсортированный массив и число. Вернуть индексы двух чисел из массива,
+"""Дан отсортированный массив и число. Вернуть массив из двух чисел в заданном массиве,
 сумма которых максимально приближена к заданному числу."""
 
 
 def two_sum(nums, target):
     left = 0
     right = len(nums) - 1
-    res = []
+    res = [nums[left], nums[right]]
+
     while left < right:
         sum_nums = nums[left] + nums[right]
 
-        if target - sum_nums < sum(res):
-            res = [left, right]
+        if abs(target - sum_nums) < abs(target - sum(res)):
+            res = [nums[left], nums[right]]
 
         if sum_nums < target:
             left += 1
@@ -21,4 +22,4 @@ def two_sum(nums, target):
     return res
 
 
-print(two_sum(nums=[-10, -5, 2, 2, 11, 13, 15], target=9))
+print(two_sum(nums=[-10, -5, -2, 2, 2, 11, 13, 15], target=9))
