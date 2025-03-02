e120268a3408760080c5ad6bb97a2a6ac9ea30da
AlexeyShchukin <shchukin.developer@gmail.com>
2025-03-02 07:58:22 +0100

    two sum 2

---
+++ two_sum_2.py
@@ -0,0 +1,28 @@
+"""Two Integer Sum II
+
+Given an array of integers numbers that is sorted in non-decreasing order.
+Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up to a given
+target number target and index1 < index2. Note that index1 and index2 cannot be equal,
+therefore you may not use the same element twice.
+There will always be exactly one valid solution.
+
+Your solution must use
+O(1) additional space."""
+
+
+class Solution:
+    def two_sum(self, numbers: list[int], target: int) -> list[int]:
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum > target:
+                right -= 1
+            elif current_sum < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
+
+        return []
