0423d8e88a584bf541a0297a1b8432e6e93b02fc
AlexeyShchukin <shchukin.developer@gmail.com>
2025-03-05 17:10:17 +0100

    reverse linked list

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -13,10 +13,15 @@ from typing import Optional
 class Solution:
     def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """Given the head of a singly linked list, reverse the list, and return the reversed list."""
-        next_node = None
-        while head:
-            new_head = head.next
-            head.next = next_node
-            next_node = head
-            head = new_head
-        return next_node
+        # создаем три указателя предыдущий (None), текущий (head) и следующий элемент (None)
+        prev = None
+        curr = head
+
+        while curr is not None:
+            next_node = curr.next  # запоминаем следующий элемент
+
+            curr.next = prev  # разворачиваем стрелку в обратную сторону
+            prev = curr  # смещаем указатели вправо
+            curr = next_node
+
+        return prev  # если curr указывает уже на пустой элемент, вернем последний prev
